d95a403564e8be7d62fc85a10cb24a7b13d4ffb5
Orguntay <67823328+Orguntay@users.noreply.github.com>
2021-01-18 00:09:13 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] kurslar = new string[]
+            {
+                "Yazılım Geliştirme Kampı",
+                "Programlamaya başlangıç için temel kurs",
+                "java kursu"
+            };
+            for (int i = 1; i < 10; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Type safety - tip güvenliği
+            // do not repat yourself - kendini tekrarlama
+            // kategoriEtiketi bir değer tutucudur.Alians
+            string kategoriEtiketi = "Kategoriler";
+
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
