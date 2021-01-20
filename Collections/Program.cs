bc2ee1c3368fc57f66600eab99d3db3bbd2c7536
Orguntay <67823328+Orguntay@users.noreply.github.com>
2021-01-20 23:50:31 +0300

    4. ders ortasi

---
+++ Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            isimler = new string[5];// referans tip olduğu için eski belleğe ulaşılamaz
+            isimler[4] = "Error";
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);// yeni adres boş
+
+            List<string> isimler2 = new List<string> { "Engin1", "Murat1", "Kerem1", "Halil1" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Success");
+            Console.WriteLine(isimler2[4]);
+
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generic class'lar tip belirtilmeden new'lenemez
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Celal");
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
